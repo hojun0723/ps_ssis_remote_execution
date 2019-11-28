89d81a59c5ae0b0e9187c6336ff0b799149a3c42
unknown <hojun0723@gmail.com>
2019-11-28 18:21:30 +0900

    first commit

---
+++ remote_execution_v1.ps1
@@ -0,0 +1,44 @@
+﻿#=======================================================================
+#
+# SSIS 패키지 CLI 환경 실행 (리모트)
+# - 패키지 실행 자동화 테스트
+#
+#=======================================================================
+
+# Variables
+$SSISNamespace = "Microsoft.SqlServer.Management.IntegrationServices"
+$TargetServerName = "localhost"
+$TargetFolderName = "C:\COMPANY\ETL\"
+$ProjectName = "Integration Services Project1"
+$PackageName = "PKG_MART_TMMNG001.dtsx"
+
+# Load the IntegrationServices assembly
+$loadStatus = [System.Reflection.Assembly]::Load("Microsoft.SQLServer.Management.IntegrationServices, "+
+    "Version=14.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91, processorArchitecture=MSIL")
+
+# Create a connection to the server
+$sqlConnectionString = `
+    "Data Source=" + $TargetServerName + ";Initial Catalog=master;Integrated Security=SSPI;"
+    #"Data Source=" + $TargetServerName + ";Initial Catalog=master;User ID=etladmin;Password=<PAss12#$>;"
+$sqlConnection = New-Object System.Data.SqlClient.SqlConnection $sqlConnectionString
+
+# Create the Integration Services object
+$integrationServices = New-Object $SSISNamespace".IntegrationServices" $sqlConnection
+
+# Get the Integration Services catalog
+$catalog = $integrationServices.Catalogs["SSISDB"]
+
+# Get the folder
+$folder = $catalog.Folders[$TargetFolderName]
+
+# Get the project
+$project = $folder.Projects[$ProjectName]
+
+# Get the package
+$package = $project.Packages[$PackageName]
+
+Write-Host "Running " $PackageName "..."
+
+$result = $package.Execute("false", $null)
+
+Write-Host "Done."
